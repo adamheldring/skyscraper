06a84148904e1ff62d266e3ba5d83441619b0727
A-Game <javascript@adamheldring.com>
2019-04-12 16:20:26 +0200

    init backend

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+import { getHTML } from "./lib/scraper";
+
+console.log(getHTML());
+
+// 5.30 in i WES BOS Scraper Day 1
---
+++ backend/lib/scraper.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const getHTML = url => {
+  const html = "cool";
+  return html;
+};
+
+export { getHTML };
